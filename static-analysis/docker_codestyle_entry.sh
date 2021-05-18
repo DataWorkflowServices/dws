a0ff21b2a0abf80ce4fd8eeb81e458f55d696edb
Anthony Floeder <anthony.floeder@hpe.com>
2021-05-18 15:57:04 -0500

    Corrections to display files that need go fmt

--- static-analysis/docker_codestyle_entry.sh
+++ static-analysis/docker_codestyle_entry.sh
@@ -10,40 +10,43 @@ fi
 
 go get -u golang.org/x/lint/golint
 if [ $? -ne 0 ] ; then
-	echo "pre-run setup error"
+	echo "pre-run setup error, unable to fetch golint"
 	exit 1
 fi
 
-GOLINTBIN=$(dirname $(go list -f {{.Target}} golang.org/x/lint/golint))
+GOLINTBIN=$(dirname "$(go list -f "{{.Target}}" golang.org/x/lint/golint)")
 export PATH=$PATH:$GOLINTBIN
 
 # Check non-vendor packages
-mypkgs=$(go list -f {{.Dir}} ./...)
-if [ -z ${mypkgs} ] ; then
-	echo "Exiting..No packages to check"
+mypkgs=$(go list -f "{{.Dir}}" ./...)
+if [ -z "${mypkgs}" ] ; then
+	echo "Exiting: No packages to check"
 	exit 0
 fi
-echo "Checking non-vendor packages:"
-echo "${mypkgs}"
 
-echo "Checking code format."
-formatResult=$(gofmt -l ${mypkgs})
-echo "format results: ${formatResult}"
-if [ ! -z ${formatResult} ] ; then
-	echo "Failed: Invalid go source file formatting detected."
-	for fileName in ${formatResult}; do
-		echo "${fileName}"
-	done
+# Check formatting wiithin those packages
+echo "Checking non-vendor packages - code format"
+foundCount=0
+for p in $mypkgs; do
+	# If gofmt returns the file name, there is a formatting issue with it.
+	formatResult=$(gofmt -l "$p")
+	if [ -n "$formatResult" ] ; then
+		echo "go fmt needed:  $formatResult"
+		foundCount=$((foundCount + 1))
+	fi
+done
+
+if [ $foundCount -ne 0 ] ; then
+	echo "go fmt failed"
 	exit 1
 fi
 
 # Check non-vendor package files ignoring all generated files
 echo "Running golint"
-for pkg in ${mypkgs}; do
-	mypkgfiles=$(find ${pkg##*dws-operator/} -maxdepth 1 -type f \( ! -iname "*zz_generated*" \))
-	echo "Checking:"
-	echo "$mypkgfiles"
-	golint -min_confidence 0.8 -set_exit_status $mypkgfiles
+for pkg in $mypkgs; do
+	mypkgfiles=$(find "${pkg##*dws-operator/}" -maxdepth 1 -type f \( ! -iname "*zz_generated*" \))
+	printf "checking $mypkgfiles\n"
+	golint -min_confidence 0.8 -set_exit_status "$mypkgfiles"
 	if [ $? -ne 0 ] ; then
 		echo "go lint failed"
 		exit 1
