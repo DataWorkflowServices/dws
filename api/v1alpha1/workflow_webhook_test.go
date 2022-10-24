66f02c1bf6b01823a9190628ff13fca2534db1b7
Dean Roehrich <dean.roehrich@hpe.com>
2022-10-24 13:17:47 -0500

    remove unused import
    Signed-off-by: Dean Roehrich <dean.roehrich@hpe.com>

--- api/v1alpha1/workflow_webhook_test.go
+++ api/v1alpha1/workflow_webhook_test.go
@@ -27,7 +27,6 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // These tests are written in BDD-style using Ginkgo framework. Refer to
