79e6427fd472505fae84816315ce01eeb4bbc905
Dean Roehrich <dean.roehrich@hpe.com>
2024-08-20 16:20:49 -0500

    Add comment markers for the crd-bumper tool (#219)
    This tool uses "marker comments" in the Go code to indicate where generated
    code should be placed.  These markers are used the same way `controller-gen`
    uses code markers.
    
    Signed-off-by: Dean Roehrich <dean.roehrich@hpe.com>

--- internal/controller/suite_test.go
+++ internal/controller/suite_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2021-2023.
+Copyright 2021-2024.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -145,6 +145,8 @@ var _ = BeforeSuite(func() {
 
 	err = (&dwsv1alpha2.Workflow{}).SetupWebhookWithManager(k8sManager)
 	Expect(err).ToNot(HaveOccurred())
+
+	// +crdbumper:scaffold:builder
 
 	// start reconcilers
 
